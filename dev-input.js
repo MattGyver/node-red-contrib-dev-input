734496a5ba186c6abadae8374b642ea6cb0d2704
Mattias Nord <mattias@abhb.se>
2016-01-09 17:18:50 +0100

    Update html to show the correct information. Update js to capture event10 which is my BT speakers remote control buttons.

--- dev-input.js
+++ dev-input.js
@@ -1,11 +1,30 @@
 module.exports = function(RED) {
-    function DevInput(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        this.on('input', function(msg) {
-//            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("dev-input",DevInput);
+
+  var msg = { topic:"dev-input" }
+
+  function parseBuffer( buffer ) {
+    var eventObject = {};
+
+    eventObject.tv_sec   = buffer.readInt32LE(0),
+    eventObject.tv_usec  = buffer.readInt32LE(8),
+    eventObject.type     = buffer.readUInt16LE(16),
+    eventObject.code     = buffer.readUInt16LE(18),
+    eventObject.value    = buffer.readInt32LE(20);
+
+    return eventObject;
+  }
+
+  function DevInput(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
+
+    var FS = require('fs');
+
+    FS.createReadStream('/dev/input/event10').on('data', function( buffer ) {
+      msg.payload = parseBuffer(buffer)
+      node.send(msg);
+    });
+
+  }
+  RED.nodes.registerType("dev-input",DevInput);
 }
